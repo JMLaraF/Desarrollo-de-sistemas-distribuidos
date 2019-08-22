9d1bce2622a6af3f682be195041969bb5e47aa34
izaird <iza_alex3@hotmail.com>
2019-08-22 11:07:03 -0500

    algunos metodos

--- Practica_01/reloj.py
+++ Practica_01/reloj.py
@@ -1,13 +1,61 @@
+import time
+from datetime import datetime
+import random 
+import math
+now = datetime.now() # current date and time
+
 class reloj():
-    activo = True
+    def __init__(self):
+        self.activo = True
+        self.hours = 0
+        self.minutes = 0
+        self.seconds = 0
+
+    def random_time(self):
+        self.hours = math.floor(random.uniform(0,23))
+        self.minutes = math.floor(random.uniform(0, 59))
+        self.seconds = math.floor(random.uniform(0,59))
+
+    def local_time(self):
+        self.hours = int(now.strftime("%H"))
+        self.minutes = int(now.strftime("%M"))
+        self.seconds = int(now.strftime("%S"))
+
+    def reset_time(self):
+        self.hours = 0
+        self.minutes = 0
+        self.seconds = 0
+    
+    def change_s(self,s):
+        self.seconds= s
+
+    def change_m(self,m):
+        self.minutes= m
+        
+    def change_h(self,h):
+        self.hours= h
+    
+
 
     def change_status(self):
-        if self.activo == False:
-            self.activo = True
-        else:
+        if (self.activo):
             self.activo = False
-    
+        else:
+            self.activo = True
+
     def show_status(self):
+        if (self.activo):
+            print(str(self.hours).zfill(2) + ":" +str(self.minutes).zfill(2)+ ":" +str(self.seconds).zfill(2))
+        else:
+            print("El reloj esta desactivado")
 
 
-reloj_01=reloj()
+
+reloj01 = reloj()
+reloj01.show_status()
+reloj01.random_time()
+reloj01.show_status()
+reloj01.local_time()
+reloj01.show_status()
+reloj01.change_h(0)
+reloj01.show_status()
